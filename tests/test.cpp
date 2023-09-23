8184c50b8939723caab7775e5a0f4400d86b0946
Raphael Dohnalek <r.dohnalek@gmx.at>
2023-09-23 23:12:06 +0200

    Add new tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -54,7 +54,82 @@ TEST_CASE_TEMPLATE("Construct basic convex hull", T, quickhull, giftwrapping)
     CHECK(lists_contain_equal_elements(expected, calculated));
 }
 
-TEST_CASE_TEMPLATE("Convex hull as line", T, quickhull, giftwrapping) 
+TEST_CASE_TEMPLATE("Nearly colinear line", T, quickhull, giftwrapping) 
+{
+    std::vector<Vector2> points = {
+        Vector2(0, 0),
+        Vector2(1, 1),
+        Vector2(1.0000001, 1.0000001),
+        Vector2(2, 2),
+        Vector2(2.0000001, 2.0000001),
+        Vector2(3, 3),
+    };
+
+    std::vector<Vector2> expected = {
+        Vector2(0, 0),        
+        Vector2(3, 3),
+    };
+
+    std::unique_ptr<T> algorithm = std::make_unique<T>();
+
+    auto calculated = algorithm->Execute(points);
+
+    CHECK(lists_contain_equal_elements(expected, calculated));
+}
+
+TEST_CASE_TEMPLATE("Nearly colinear hull", T, quickhull, giftwrapping) 
+{
+    std::vector<Vector2> points = {
+        Vector2(0, 0),
+        Vector2(1, -5),
+        Vector2(1, -4.99999999999),
+        Vector2(1, 0),
+        Vector2(1, 4.999999999999),
+        Vector2(1, 5),
+        Vector2(2, 0)
+    };
+
+    std::vector<Vector2> expected = {
+        Vector2(0, 0),
+        Vector2(1, -5),
+        Vector2(1, 5),
+        Vector2(2, 0)
+    };
+
+    std::unique_ptr<T> algorithm = std::make_unique<T>();
+
+    auto calculated = algorithm->Execute(points);
+
+    CHECK(lists_contain_equal_elements(expected, calculated));
+}
+
+TEST_CASE_TEMPLATE("Different y levels", T, quickhull, giftwrapping) 
+{
+    std::vector<Vector2> points = {
+        Vector2(0, 0),
+        Vector2(1, -5),
+        Vector2(1, -4.5),
+        Vector2(1, 0),
+        Vector2(1, 4.5),
+        Vector2(1, 5),
+        Vector2(2, 0)
+    };
+
+    std::vector<Vector2> expected = {
+        Vector2(0, 0),
+        Vector2(1, -5),
+        Vector2(1, 5),
+        Vector2(2, 0)
+    };
+
+    std::unique_ptr<T> algorithm = std::make_unique<T>();
+
+    auto calculated = algorithm->Execute(points);
+
+    CHECK(lists_contain_equal_elements(expected, calculated));
+}
+
+TEST_CASE_TEMPLATE("Colinear points", T, quickhull, giftwrapping) 
 {
     std::vector<Vector2> points = {
         Vector2(0, 0),
@@ -66,6 +141,31 @@ TEST_CASE_TEMPLATE("Convex hull as line", T, quickhull, giftwrapping)
     std::vector<Vector2> expected = {
         Vector2(0, 0),        
         Vector2(3, 3),
+    };
+
+    std::unique_ptr<T> algorithm = std::make_unique<T>();
+
+    auto calculated = algorithm->Execute(points);
+
+    CHECK(lists_contain_equal_elements(expected, calculated));
+}
+
+TEST_CASE_TEMPLATE("Duplicate points", T, quickhull, giftwrapping) 
+{
+    std::vector<Vector2> points = {
+        Vector2(0, 0),
+        Vector2(0, 0),
+        Vector2(1, -5),
+        Vector2(1, 0),
+        Vector2(1, 5),
+        Vector2(2, 0)
+    };
+
+    std::vector<Vector2> expected = {
+        Vector2(0, 0),
+        Vector2(1, -5),
+        Vector2(1, 5),
+        Vector2(2, 0)
     };
 
     std::unique_ptr<T> algorithm = std::make_unique<T>();
