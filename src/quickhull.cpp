5f5413ea873efe045a90b3daa804032c6f2ab430
Raphael Dohnalek <r.dohnalek@gmx.at>
2023-09-25 22:56:42 +0200

    Fixed imports and const errors

--- src/quickhull.cpp
+++ src/quickhull.cpp
@@ -1,4 +1,5 @@
 #include "quickhull.h"
+#include <math.h>
 
 std::string quickhull::getName() const
 {
@@ -8,14 +9,13 @@ std::string quickhull::getName() const
 std::vector<Vector2> quickhull::Execute(const std::vector<Vector2> &points) const
 {
     if (points.size() < 3) {
-        std::cout << "Not enough points to form a convex hull." << std::endl;
-        return std::vector<Vector2>();
+        return points;
     }
 
     std::vector<Vector2> convexHull;
     // Find the initial leftmost and rightmost points
     int min_x = 0, max_x = 0;
-    for (int i = 1; i < points.size(); i++) {
+    for (size_t i = 1; i < points.size(); i++) {
         if (points[i].x < points[min_x].x)
             min_x = i;
         if (points[i].x > points[max_x].x)
@@ -23,22 +23,24 @@ std::vector<Vector2> quickhull::Execute(const std::vector<Vector2> &points) cons
     }
 
     // Compute the convex hull using the QuickHull algorithm
-    quickHull(points, points[min_x], points[max_x], convexHull);
+    this->quickHull(points, points[min_x], points[max_x], convexHull);
 
     return convexHull;
 }
 
-double quickhull::findDistance(Vector2 p1, Vector2 p2, Vector2 p) {
+double quickhull::findDistance(Vector2 p1, Vector2 p2, Vector2 p) const
+{
     double numerator = std::abs((p.x - p1.x) * (p2.y - p1.y) - (p.y - p1.y) * (p2.x - p1.x));
     double denominator = std::sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
     return numerator / denominator;
 }
 
-void quickhull::quickHull(const std::vector<Vector2> &points, Vector2 p1, Vector2 p2, std::vector<Vector2> &result) {
+void quickhull::quickHull(const std::vector<Vector2> &points, Vector2 p1, Vector2 p2, std::vector<Vector2> &result) const
+{
     int index = -1;
     double maxDistance = 0.0;
 
-    for (int i = 0; i < points.size(); i++) {
+    for (size_t i = 0; i < points.size(); i++) {
         double distance = findDistance(p1, p2, points[i]);
         if (distance > maxDistance) {
             maxDistance = distance;
@@ -55,7 +57,7 @@ void quickhull::quickHull(const std::vector<Vector2> &points, Vector2 p1, Vector
     Vector2 A = points[index];
     std::vector<Vector2> leftSet, rightSet;
 
-    for (int i = 0; i < points.size(); i++) {
+    for (size_t i = 0; i < points.size(); i++) {
         if (findDistance(p1, A, points[i]) > 0)
             leftSet.push_back(points[i]);
         else if (findDistance(A, p2, points[i]) > 0)
--- src/quickhull.h
+++ src/quickhull.h
@@ -8,6 +8,6 @@ public:
     virtual std::vector<Vector2> Execute(const std::vector<Vector2>& points) const override;
 
 private:
-    double findDistance(Vector2 p1, Vector2 p2, Vector2 p);
-    void quickHull(const std::vector<Vector2> &points, Vector2 p1, Vector2 p2, std::vector<Vector2>& result);
+    double findDistance(Vector2 p1, Vector2 p2, Vector2 p) const;
+    void quickHull(const std::vector<Vector2> &points, Vector2 p1, Vector2 p2, std::vector<Vector2> &result) const;
 };
