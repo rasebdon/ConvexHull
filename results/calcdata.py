e206a03bb06500cd7e136f88d9712dcdb35cb6d6
Raphael Dohnalek <r.dohnalek@gmx.at>
2023-10-04 23:20:40 +0200

    Add test results

--- results/calcdata.py
+++ results/calcdata.py
@@ -1,21 +1,20 @@
 import numpy as np
 import pandas as pd
-
+from os import listdir
+from os.path import isfile, join
 
 def print_results(path):
     data = pd.read_csv(path)
 
     print(f"Algo={data.iloc[0].iloc[0]}")
     print(f"N={data.iloc[0].iloc[1]}")
-    print(f"Mean {data.loc[:, 'ms'].mean()}")
-    print(f"Min {data.loc[:, 'ms'].min()}")
-    print(f"Max {data.loc[:, 'ms'].max()}")
+    print(f"Mean {data.loc[:, 'ms'].mean() / 1000}".replace(".", ","))
+    print(f"Max {data.loc[:, 'ms'].max() / 1000}".replace(".", ","))
+    print(f"Min {data.loc[:, 'ms'].min() / 1000}".replace(".", ","))
+    print("----------------------------------------------")
 
+dirName = "./line"
+files = [f for f in listdir(dirName) if isfile(join(dirName, f))]
 
-print_results("./results_qh_1000.csv")
-print_results("./results_qh_10000.csv")
-print_results("./results_qh_100000.csv")
-print_results("./results_qh_250000.csv")
-print_results("./results_qh_500000.csv")
-print_results("./results_qh_1000000.csv")
-print_results("./results_qh_10000000.csv")
+for file in files:
+    print_results(f"{dirName}/{file}")
